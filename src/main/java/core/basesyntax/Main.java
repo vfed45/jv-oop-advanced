86b899d2c3eb471391aa553c3f712448c1361a87
Viktoriia <fvee737@gmail.com>
2025-09-27 14:59:54 +0200

    feat: created the main class

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,19 @@
+package core.basesyntax;
+
+public class Main {
+    public static void main(String[] args) {
+        int arraySize = 6;
+        Figure[] figures = new Figure[arraySize];
+        FigureSupplier figureSupplier = new FigureSupplier();
+        for (int i = 0; i < arraySize / 2; i++) {
+            figures[i] = figureSupplier.getRandomFigure();
+        }
+        for (int i = arraySize / 2; i <arraySize; i++) {
+            figures[i] = figureSupplier.getDefaultFigure();
+        }
+        System.out.println("List of all figures:");
+        for (Figure figure : figures) {
+            figure.draw();
+        }
+    }
+}
