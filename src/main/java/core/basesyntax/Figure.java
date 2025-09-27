c524ba33a1c0d5ad547f9ad59ee963d81be53f29
Viktoriia <fvee737@gmail.com>
2025-09-27 16:09:25 +0200

    added interface AreaCalculatable

--- src/main/java/core/basesyntax/Figure.java
+++ src/main/java/core/basesyntax/Figure.java
@@ -1,6 +1,6 @@
 package core.basesyntax;
 
-public abstract class Figure implements Drawable {
+public abstract class Figure implements Drawable, AreaCalculatable {
     protected String color;
 
     public Figure(String color) {
